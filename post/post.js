23731bcd572b7b4fed52d16023794517091155a9
jamesj0918 <jamesj0107@naver.com>
2021-02-06 22:10:28 +0900

    Basic CRUD operations on Post (except update)

--- post/post.js
+++ post/post.js
@@ -1,35 +1,61 @@
-const { response } = require('express');
 const express = require('express');
+const router = express.Router();
 
 const _query = require('../database/db');
 const utils = require('../utils/utils');
 
-const router = express.Router();
+let query_response =  {status: '200 OK'};
 
 
 router.use((req, res, next) =>{
     // Middleware goes here
-    console.log(`${req.ip} requested on ${req.path}`);
+    console.log(`${req.method}  ${req.ip} requested on ${req.path}`);
     next();
 });
 
 router.get('/post', async (req, res) => {
-    let data = await _query('SELECT * FROM Post;');
-    res.send(data);
+    try {
+        query_response.data = await _query('SELECT * FROM Post;');
+    }
+    catch (error) {
+        query_response.status = '400 Bad Request';   
+        query_response.data = error;     
+    }
+    
+    res.send(query_response);
 });
 
 router.post('/post', async (req, res) => {
-    let query_response =  {status: '200 OK'};
     query_response.data = req.body;
+
     try {
-        await _query(utils._insert('post', req.body));    
+        await _query(utils._insert(table='post', req.body));    
+    }
+    catch (error) {
+        query_response.status = '400 Bad Request';   
+        query_response.data = error;     
+    }
+
+    res.send(query_response);
+;})
+
+router.delete('/post/:id', async (req, res) => {
+    try {
+        let query = await _query(utils._select('post', req.params.id));
+        if (query.length == 0) {
+            query_response.status = '204 No Content ';   
+            query_response.data = `Post with id=${req.params.id} does not exists.`;
+        }
+        else {
+            await _query(utils._delete('post', req.params.id));
+            query_response.data = `${req.params.id} has been successfully deleted.`;
+        }
     }
     catch (error) {
         query_response.status = '400 Bad Request';   
         query_response.data = error;     
     }
     res.send(query_response);
-;})
-
+});
 
 module.exports = router;
--- utils/utils.js
+++ utils/utils.js
@@ -4,14 +4,21 @@ const _response = (status, body) =>{
     res.body = body;
 
     return res
-}
+};
 
 const _insert = (table, request) => {
     const columns = Object.keys(request);
     const values = Object.values(request).map(value => `'${value}'`);
 
     return query = `INSERT INTO ${table} (${columns.join(', ')}) VALUES (${values.join(', ')});`;
-}
+};
 
+const _delete = (table, id) => {
+    return query = `DELETE FROM ${table} WHERE id='${id}';`;
+};
 
-module.exports = { _response, _insert }
+const _select = (table, id) => {
+    return query = `SELECT * FROM ${table} WHERE id=${id};`;
+};
+
+module.exports = { _response, _insert, _delete, _select }
