86ca87ef5845668b986091f852228c9e1c9d5a0b
jamesj0918 <jamesj0107@naver.com>
2021-02-06 18:18:36 +0900

    Change DB library to mysql2

--- database/db.js
+++ database/db.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database: 'test_db'
-  });
-
-  db.connect();
-  
-  module.exports = db;
+const _query = async (raw_query) => {
+    const connection = await mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'password',
+        database: 'test_db'
+    });
+    let [rows, _] = await connection.execute(raw_query);
+    await connection.end();
+    return rows;
+};
+module.exports = _query;
